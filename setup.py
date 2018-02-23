a5bc354ff5ad19e129773a93703fbf69d22daf83
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-02-23 18:57:53 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
-import os.path
-import re
 from setuptools import setup
+from pathlib import Path
+
+import snug
 
 
 def read(fname):
-    path = os.path.join(os.path.dirname(__file__), fname)
-    with open(path, 'r') as rfile:
-        return rfile.read()
-
-
-def find_version(path):
-    version_file = read(path)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    return (Path(__file__).parent / fname).read_text()
 
 
 setup(
     name='snug',
-    version=find_version('snug.py'),
-    description='Write reusable web API interactions',
+    version=snug.__version__,
+    description=snug.__description__,
     license='MIT',
     long_description=read('README.rst') + '\n\n' + read('HISTORY.rst'),
     url='https://github.com/ariebovenberg/snug',
 
-    author='Arie Bovenberg',
+    author=snug.__author__,
     author_email='a.c.bovenberg@gmail.com',
 
     classifiers=[
@@ -35,16 +25,12 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    install_requires=[
-        'typing>=3.6.2;python_version<"3.5"'
-    ],
     keywords=['api-wrapper', 'http', 'generators', 'async',
               'graphql', 'rest', 'rpc'],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     py_modules=('snug',),
 )
